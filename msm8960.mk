ad1838f7af53c5bd0a596cafd2f85a739492602c
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-12 17:07:18 +0530

    Update media_codecs for L
    Change-Id: I8d6aa6e94c997c580e027b69ca137a6db1163774

--- msm8960.mk
+++ msm8960.mk
@@ -18,5 +18,8 @@ $(call inherit-product, device/sony/qcom-common/qcom-common.mk)
 COMMON_PATH := device/sony/msm8960-common
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
