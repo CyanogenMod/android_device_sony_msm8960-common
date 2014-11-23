660f88ef169c01865a97141b3cdc75aab90e0d62
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-23 10:53:11 +0530

    Add lights wrapper
    * Moves as-is from sony/qcom-common
    
    Change-Id: I9147ddf08bb01dda1f1008f465f8fd89021f1ff1

--- msm8960.mk
+++ msm8960.mk
@@ -23,3 +23,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8960
