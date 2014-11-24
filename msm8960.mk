021137184fd839f19f0b50efff8532c3470055b3
Varun Chitre <varun.chitre15@gmail.com>
2014-11-24 16:48:04 +0100

    msm8960-common: add qcmediaplayer
    PS2: fix typo, add description
    
    Change-Id: I399f311c198357585ef3f209fdd5cc19ec37e6ba

--- msm8960.mk
+++ msm8960.mk
@@ -17,12 +17,20 @@ $(call inherit-product, device/sony/qcom-common/qcom-common.mk)
 
 COMMON_PATH := device/sony/msm8960-common
 
+# Lights wrapper
+PRODUCT_PACKAGES += \
+    lights.qcom
+
+# Media profile
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
-# Lights wrapper
+# Media
 PRODUCT_PACKAGES += \
-    lights.qcom
+    qcmediaplayer
+
+PRODUCT_BOOT_JARS += \
+    qcmediaplayer
