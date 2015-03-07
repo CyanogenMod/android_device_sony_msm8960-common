4e07db1065ada79884c2e745ee66ba025c544871
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-07 21:42:17 +0100

    remove dependency on qcom-common, consolidate common definitions
    Change-Id: Ic1120eb60089906484f651ba0714bc0148bc1e92

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,16 +12,59 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# inherit from qcom-common
-include device/sony/qcom-common/BoardConfigCommon.mk
+# Inherit from Sony common
+include device/sony/common/BoardConfigCommon.mk
 
+# Include path
 TARGET_SPECIFIC_HEADER_PATH += device/sony/msm8960-common/include
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
 
 # Bionic
 MALLOC_IMPL := dlmalloc
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Board overrides
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Camera
+TARGET_PROVIDES_CAMERA_HAL := true
+
+# Display HAL
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+TARGET_USES_C2D_COMPOSITION := true
+
+# Font expansion
+EXTENDED_FONT_FOOTPRINT := true
 
 # Lights HAL
 TARGET_PROVIDES_LIBLIGHT := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+
+# Power HAL
+TARGET_POWERHAL_VARIANT := qcom
+CM_POWERHAL_EXTENSION := qcom
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+BOARD_RIL_CLASS := ../../../device/sony/msm8960-common/ril/
