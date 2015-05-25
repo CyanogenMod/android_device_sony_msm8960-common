8c4e7444f5c2f5a3ed313892c815ab66ab5b3245
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-05-25 08:49:50 +0200

    build: exclude fusion3
    prevent building anything in msm8960 for fusion platform.
    it will get lp blobs, new liblights, new mac config
    
    Change-Id: Ie08a2f650dd9fc45639653d6efd611d7d13e7ca0

--- Android.mk
+++ Android.mk
@@ -2,6 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(BOARD_VENDOR_PLATFORM),fusion3)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
 endif
