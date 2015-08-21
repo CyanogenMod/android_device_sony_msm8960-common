235b4663607915fa61b8028cff1219207ed253db
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-08-21 10:09:37 +0200

    build: exclude blue
    Change-Id: Ie37643a899466aebeba653043b441d35db07d57a

--- Android.mk
+++ Android.mk
@@ -3,7 +3,9 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifneq ($(BOARD_VENDOR_PLATFORM),fusion3)
+ifneq ($(BOARD_VENDOR_PLATFORM),blue)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
 endif
 endif
