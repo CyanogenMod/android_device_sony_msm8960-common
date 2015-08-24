fc1190bcf02609102126e2f9f8ee5ddb5f2b7ecd
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-08-24 08:36:19 +0200

    build: exclude taoshan
    Change-Id: I4816be9904f0d95ab6bba3f0baf0d08467c10996

--- Android.mk
+++ Android.mk
@@ -4,7 +4,9 @@ ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifneq ($(BOARD_VENDOR_PLATFORM),fusion3)
 ifneq ($(BOARD_VENDOR_PLATFORM),blue)
+ifneq ($(BOARD_VENDOR_PLATFORM),taoshan)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
 endif
 endif
